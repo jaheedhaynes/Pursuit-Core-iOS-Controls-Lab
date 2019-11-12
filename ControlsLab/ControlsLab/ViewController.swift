52e243ef1f22220a1ac3ea41abbc205ffce0288a
Jaheed Haynes <jaheedhaynes@pursuit.org>
2019-11-12 06:35:07 -0500

    update

--- ControlsLab/ControlsLab/ViewController.swift
+++ ControlsLab/ControlsLab/ViewController.swift
@@ -2,9 +2,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cardValueSignOutlet: UISegmentedControl!
+    
+    @IBOutlet weak var stepperOutlet: UIStepper!
+    
+    @IBOutlet weak var cardImageView: UIImageView!
+    
+    
+    var selectedSuit : Int = 0 {
+        didSet {
+            switch cardValueSignOutlet.selectedSegmentIndex {
+            case 0 :
+                cardImageView.image = #imageLiteral(resourceName: "twoOfClubs")
+            case 1 :
+                cardImageView.image = #imageLiteral(resourceName: "twoOfHearts")
+            case 2 :
+                cardImageView.image = #imageLiteral(resourceName: "twoOfClubs")
+            case 3 :
+                cardImageView.image = #imageLiteral(resourceName: "twoOfDiamons")
+            default :
+               print("something else happened")
+            }
+        }
+    }
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
     }
 }
 
